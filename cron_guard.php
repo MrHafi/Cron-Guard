6570f5e6e688b39e84f22d882585bb782416d345
Mr Hafi <hafi.cs01@gmail.com>
2026-02-03 15:44:06 +0500

    Added wp cron job when user hit 404 and it will add user data in table

--- cron_guard.php
+++ cron_guard.php
@@ -24,3 +24,44 @@ function cron_guard_enqueue_scripts() {
     );
 }
 add_action('wp_enqueue_scripts', 'cron_guard_enqueue_scripts');
+
+
+
+
+
+
+// ////////////////// WHEN USER HIT 404 //////////////////////////////////////////////
+// STEP 2: CHECKING 404 HITS
+add_action('template_redirect', function(){
+
+if(is_404()){
+    if(!wp_next_scheduled( 'log_404_cron' ))
+        {
+            wp_schedule_single_event(  time(),'log_404_cron' );
+        }
+}
+} );
+
+// triggering cron
+add_action( 'log_404_cron', 'hit_404');
+function hit_404(){
+
+// AVOIDE TWICE DATA ENTRY AS DUROING CRON IT DOES ADD TWICE
+ if ( defined('DOING_CRON') && DOING_CRON ) {
+        return;
+    }
+
+// INSERTING IN DB
+global $wpdb;
+
+$wpdb->insert(
+      $wpdb->prefix . 'cron_guard_events',
+    [
+        'event_type' => '404',
+        'ip'         => $_SERVER['REMOTE_ADDR'],
+        'url'        => esc_url_raw($_SERVER['REQUEST_URI']),
+        'created_at' => current_time('mysql')
+    ],
+    [ '%s', '%s', '%s', '%s' ]
+);
+}
